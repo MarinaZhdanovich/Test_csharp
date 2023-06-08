2b8ad32f61bfa6b50512ae39c5e981dbb0ca53d5
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2023-06-08 19:43:16 +0300

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "Marina")
+{
+    Console.WriteLine("This is Marina!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}

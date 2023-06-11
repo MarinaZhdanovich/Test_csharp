84f7cf6a075ee318f4f6cd91b6586ffcfcdea674
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2023-06-11 22:10:13 +0300

    Array and break

---
+++ Task15/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = {1, 12, 31, 4, 15, 16, 17, 15, 18};
+
+int n = array.Length; // количество элементов массива
+int find = 15; //элемент 15
+int index = 0; //позиция 4
+
+while(index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //находит только первый повторяющийся элемент
+    }
+    index++;
+}

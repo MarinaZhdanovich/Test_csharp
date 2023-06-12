3bcb47aba00f2f7dda635ff3bbad2a6d80d183db
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2023-06-12 20:08:57 +0300

    Void 2

---
+++ Task17/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int [] collection)   // метод не возвращает
+{
+    int length = collection.Length; //длина массива
+    int index = 0; //позиция
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int [] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int [] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // 0 - сохраняем позицию, -1 - если элемента нет 
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index; // в позицию значение индекса
+            break; //так как находит последнее вхождение, сейчас будет первое
+        }
+        index++;
+    }
+    return position;
+}
+
+int [] array = new int [10]; // создает массив с 10 элементами, по умолчанию будут нули
+// определилили
+
+FillArray(array); // заполнили массив
+PrintArray(array); // распечатали массив
+Console.WriteLine(); // не является частью вывода массива
+
+int pos = IndexOf(array, 45); 
+Console.WriteLine(pos);

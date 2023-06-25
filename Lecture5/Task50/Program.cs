424d8dcfea763db417a8c8d882f53639123f4297
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2023-06-25 20:53:19 +0300

    Lesson5

---
+++ Lecture5/Task50/Program.cs
@@ -0,0 +1,28 @@
+﻿void PrintArray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)  // 0 - строка
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++) // 1 столбец
+        {
+            Console.Write($" {matr[rows, columns]} "); //дополнительно поставили пробел
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
---
+++ Lecture5/Task52/Program.cs
@@ -0,0 +1,14 @@
+﻿// 5! = 5*4*3*2*1
+// 5*4!
+// 4*3!
+// 3*2!
+
+int Factorial(int n)
+{
+    if (n == 1)
+        return 1; //0! = 1, 1! = 1
+    else
+        return n * Factorial(n - 1);
+}
+
+Console.WriteLine(Factorial(3)); //1*2*3

7c56143742d13e985800d1188400747506d40e64
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2023-07-07 21:00:26 +0300

    Lesson8

---
+++ Lecture8/Task71/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine()!);
+}
+
+// string PrintNumbers(int start, int end)
+// {
+//     if (start == end) return start.ToString();
+//     return (start + " " + PrintNumbers(start + 1, end));
+// }
+
+// int num = InputNum("Введите целое число: ");
+// Console.WriteLine(PrintNumbers(1, num));
+
+void PrintNumbers(int start, int end)
+{
+    if (start > end) return; // ничего не возвращаем поэтому start > end
+    Console.WriteLine(start + " ");
+    PrintNumbers(start + 1, end);  // ++start сначала увеличиваем значение, а потом вызов функции
+}
+int num = InputNum("Введите целое число: ");
+PrintNumbers(1, num);
+
+
+// void PrintNumbers(int start, int end)
+// {
+//     if (start > end) return; // ничего не возвращаем поэтому start > end
+//     PrintNumbers(start + 1, end);  // ++start 1+2+3+4+5 стек собрался и 
+//     Console.WriteLine(start + " "); // раскручивается в обратную сторону 5 4 3 2 1
+//     
+// }
+// int num = InputNum("Введите целое число: ");
+// PrintNumbers(1, num);
---
+++ Lecture8/Task74/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8
+
+***** учесть отрицательные степени
+*/
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine()!);
+}
+
+// double Multiplication(int A, int B)
+// {
+//     if (B == 0)
+//         return 1;
+
+//     else if (B > 0)
+//     {
+//         return A * Multiplication(A, B - 1);
+//     }
+//     else
+//         return 1 / Multiplication(A, -B);  // -B (-(-2) = 2 (B становится положительным) -> if (B > 0) = 1 /  A * Multiplication(A, B - 1)
+// }
+
+// int A = InputNum("Введите целое число A: ");
+// int B = InputNum("Введите целое число B: ");
+// double result = Multiplication(A, B);
+// Console.WriteLine(result);
+
+double StepenNumber(int a, int b)
+{
+    if (b == 0) return 1;
+    if (b > 0)
+    {
+        return a * StepenNumber(a, b - 1);
+    }
+    return 1 / (a * StepenNumber(a, -b - 1));
+}
+int a = InputNum("Введите целое число A: ");
+int b = InputNum("Введите целое число B: ");
+Console.WriteLine(StepenNumber(a, b));
+
+
+/* Например, 5
+A *(A, 4)
+A * A (A, 3)
+A * A * A (A, 2)
+A * A * A * A (A, 1)
+A * A * A * A * A * (0 = 1), так как (b == 0) return 1 стек собран
+
+начинаем собирать ответ = 1 * A * A * A * A * A 
+*/

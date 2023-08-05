579105da5b1131e995876c3ddc9ccccdd0d3ed8a
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2023-08-05 21:20:00 +0300

    Added QuickSort

---
+++ Bootcamp/Sorting Quick/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] SortQuick(int[] array, int left, int right) //индекс левой границы 0 и индекс правой границы длина - 1
+{
+    int i = left;  //индекс, который будет двигаться с левого края к правому краю массива
+    int j = right; //индекс, который будет двигаться с правого края к левому краю массива
+
+    int pivot = array[Random.Shared.Next(left, right)];  //опорный элемент, случайный элемент из текущего подмассива
+    while (i <= j)
+    {
+        while (array[i] < pivot) i++;   // элементы с индексами слева от i  меньше опорного элемента
+        while (array[j] > pivot) j--;  //элементы с индексами справа от j были больше опорного элемента
+
+        if (i <= j)
+        {
+            int t = array[i];
+            array[i] = array[j];  //Если индекс i меньше или равен j, то это означает, что элемент array[i] больше или равен опорному элементу, а элемент array[j] меньше или равен опорному элементу. 
+            array[j] = t;  // элементы меняются местами, чтобы правильно разделить подмассив.
+            i++;
+            j--;
+        }
+    }
+    if (i < right) SortQuick(array, i, right); //после разделения массива на две части относительно опорного элемента, функция вызывается рекурсивно для обеих половинок.
+    if (left < j) SortQuick(array, left, j);
+    return array;
+}
+
+int[] array = { 7, 2, 1, 6, 8, 5, 3, 4 };
+Console.WriteLine("Исходный массив:");
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+SortQuick(array, 0, array.Length - 1);
+Console.WriteLine("Отсортированный массив:");
+Console.WriteLine($"[{String.Join(", ", array)}]");
---
+++ Bootcamp/Sorting_Quick/Program.cs
@@ -0,0 +1,54 @@
+﻿int[] QuickSort(int[] array)
+{
+    if (array.Length <= 1)
+    {
+        return array;
+    }
+    else
+    {
+        int pivot = array[0];
+        int countLeft = 0;
+        int countRight = 0;
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < pivot)
+            {
+                countLeft++;
+            }
+            else
+            {
+                countRight++;
+            }
+        }
+        int[] left = new int[countLeft];
+        int[] right = new int[countRight];
+
+        int indexLeft = 0;
+        int indexRight = 0;
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < pivot)
+            {
+                left[indexLeft] = array[i];
+                indexLeft++;
+            }
+            else
+            {
+                right[indexRight] = array[i];
+                indexRight++;
+            }
+        }
+        return QuickSort(left).Concat(new int[] {pivot}).Concat(QuickSort(right)).ToArray();
+    }
+}
+// После того, как левый подмассив был отсортирован, опорный элемент pivot добавляется в результирующий массив. 
+// В этой строке pivot оборачивается в новый массив типа int, чтобы можно было использовать метод Concat.
+// метод ToArray() преобразует последовательность элементов, полученных после объединения левого подмассива, 
+// опорного элемента и правого подмассива, в новый одномерный массив.
+int[] array = { 7, 2, 1, 6, 8, 5, 3, 4 };
+Console.WriteLine("Исходный массив:");
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int[] result = QuickSort(array);
+Console.WriteLine("Отсортированный массив:");
+Console.WriteLine($"[{String.Join(", ", result)}]");

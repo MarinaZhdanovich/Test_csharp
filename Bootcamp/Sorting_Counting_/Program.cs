1e2deed340fc097624498c3557d12ca6401034cf
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2023-08-09 22:13:42 +0300

    Sorting Counting

---
+++ Bootcamp/Sorting_Counting_/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] SortCounting(int[] collection)
+{
+    int size = collection.Length;
+
+    int max = collection[0];
+    for (int i = 1; i < size; i++)
+        if (collection[i] > max) max = collection[i];
+
+    int[] counter = new int[max + 1];
+
+    for (int i = 0; i < size; i++)
+        counter[collection[i]]++;
+    Console.WriteLine($"counter = [{String.Join(' ', counter)}]");
+    int index = 0;
+    for (int i = 0; i < max + 1; i++)
+        for (int j = 0; j < counter[i]; j++)
+            collection[index++] = i;
+
+    return collection;
+}
+
+int[] array = { 1, 2, 1, 6, 6, 5, 3, 4 };
+Console.WriteLine("Исходный массив:");
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int [] newArray = SortCounting(array);
+Console.WriteLine("Отсортированный массив:");
+Console.WriteLine($"[{String.Join(", ", newArray)}]");
